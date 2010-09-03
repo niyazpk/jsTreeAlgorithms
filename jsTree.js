e929fe0c6b1aacfeb43b1f9d45a4833d0585327c
Niyaz <niyazpk@gmail.com>
2010-09-03 12:41:27 +0530

    code cleanup

--- jsTree.js
+++ jsTree.js
@@ -1,12 +1,16 @@
-const INFINITY = 99999; 	//Put a large number in here.
+const INFINITY = 99999; 	// Put a large number in here.
+var isItTimeYet;			// Used to introduce delay in various methods
 
+
+// Function to get a tree of desired depth and leaves
 function jsTree(depth, leaves) {
 	var node = new Object();
-	node.value=0;
+	node.value = 0;
+	node.status = 0;
 	node.terminal = function(){return false;};
 	node.children = new Array;
 	if(depth > 0){
-		for(var i = leaves;i > 0;i--){
+		for(var i = leaves; i > 0; i--){
 			node.children.push(jsTree(depth-1, leaves));
 		}
 	}
@@ -14,6 +18,7 @@ function jsTree(depth, leaves) {
 	return node;
 }
 
+// Function to display the tree in the HTML page.
 function displayTree(node){
 	var	str = "";
 	
@@ -27,20 +32,22 @@ function displayTree(node){
 	
 	createTreeHTML(node);
 	$('#tree-container').html(str);
+
 }
 
+// Minimax algorithm implementaion
 function minimax(node, depth){
 	if((node.terminal)() == true || depth == 0)
 		return evaluate(node);
 	a = -INFINITY;
 	for(var child in node.children){
-		a = Math.max(a, -minimax(node.children[child], depth-1));	
+		a = Math.max(a, -minimax(node.children[child], depth-1));
 	}
 	node.value = a;
-	console.log(a);
 	return a;
 }
 
+// Evaluation function
 function evaluate(node){
 	return node.value;
 }
